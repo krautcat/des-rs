3c3aaff51167762ffb6dd490d44ce9e083970b41
Georgiy Odisharia <math.kraut.cat@gmail.com>
2017-04-23 16:12:47 +0300

    Changed docs

--- src/lib.rs
+++ src/lib.rs
@@ -4,13 +4,13 @@
 //!
 //! # Example
 //!
-//! ```c
+//! ```
 //! extern crate des_rs_krautcat;
 //!
 //! let key = [0x13, 0x34, 0x57, 0x79, 0x9B, 0xBC, 0xDF, 0xF1];
 //! let message = [0x01, 0x23, 0x45, 0x67, 0x89, 0xAB, 0xCD, 0xEF];
-//! let cipher = des::encrypt(&message, &key);
-//! let message = des::decrypt(&cipher, &key);
+//! let cipher = des_rs_krautcat::encrypt(&message, &key);
+//! let message = des_rs_krautcat::decrypt(&cipher, &key);
 //! ```
 //!
 //! # Usage
